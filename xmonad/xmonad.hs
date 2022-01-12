7bf1f29f45caa818412c794ee6fbe990d6e648f8
Gustavo Maciel <gmmaciel.bh2@gmail.com>
2022-01-12 13:06:32 -0300

    dmenu font size decreased

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -71,7 +71,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "dmenu_run -fn 'SFMono-10'")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
