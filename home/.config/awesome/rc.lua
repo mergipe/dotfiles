b0c5864b7d4ffb79b74e7d2b70d9a727f610b31f
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2024-10-21 14:29:12 -0300

    awesome wibar config

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -17,6 +17,7 @@ local hotkeys_popup = require 'awful.hotkeys_popup'
 -- when client with a matching name is opened:
 require 'awful.hotkeys_popup.keys'
 local lain = require 'lain'
+local markup = lain.util.markup
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -89,22 +90,51 @@ awful.layout.layouts = {
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock(' %a %d/%m/%y %H:%M:%S ', 1)
+--
+local label_color = '#9e9e9e'
+local sep_color = '#6e6e6e'
+local alert_color = '#ff2626'
+mytextclock = wibox.widget.textclock('%a %d/%m/%y %H:%M:%S', 1)
 local lain_cpu = lain.widget.cpu {
     settings = function()
-        widget:set_markup(' cpu: ' .. cpu_now.usage .. '% ')
+        local label = markup.fg.color(label_color, 'cpu')
+        local value = cpu_now.usage
+        if value > 80 then
+            value = markup.fg.color(alert_color, value)
+        end
+        widget:set_markup(label .. ' ' .. value .. '%')
     end,
 }
 local lain_mem = lain.widget.mem {
     settings = function()
-        widget:set_markup(' mem: ' .. mem_now.used .. 'M (' .. mem_now.perc .. '%) ')
+        local label = markup.fg.color(label_color, 'mem')
+        local value = mem_now.used
+        if mem_now.perc > 85 then
+            value = markup.fg.color(alert_color, value)
+        end
+        widget:set_markup(label .. ' ' .. value .. 'M(' .. mem_now.perc .. '%)')
     end,
 }
 local lain_fs = lain.widget.fs {
+    showpopup = 'off',
     settings = function()
         local root = fs_now['/']
         local hdd = fs_now['/mnt/HDD']
-        widget:set_markup(' ssd: ' .. root.percentage .. '% | hdd: ' .. hdd.percentage .. '% ')
+        local root_label = markup.fg.color(label_color, 'ssd')
+        local root_value = root.percentage
+        if root_value > 90 then
+            root_value = markup.fg.color(alert_color, root_value)
+        end
+        local text = root_label .. ' ' .. root_value .. '%'
+        if hdd then
+            local hdd_label = markup.fg.color(label_color, 'hdd')
+            local hdd_value = hdd.percentage
+            if hdd_value > 90 then
+                hdd_value = markup.fg.color(alert_color, hdd_value)
+            end
+            text = text .. ' ' .. hdd_label .. ' ' .. hdd_value .. '%'
+        end
+        widget:set_markup(text)
     end,
 }
 local lain_net = lain.widget.net {
@@ -112,18 +142,50 @@ local lain_net = lain.widget.net {
     wifi_state = 'on',
     eth_state = 'on',
     settings = function()
-        local eth = net_now.devices.enp5s0
-        -- if eth and eth.ethernet then
-        --     widget:set_markup ' cable '
-        -- end
-        local wlan = net_now.devices.wlp9s0f3u2
-        if wlan and wlan.wifi then
-            awful.spawn.easy_async('iwgetid -r', function(stdout, stderr, reason, exit_code)
-                widget:set_markup(' ' .. stdout .. ' ')
-            end)
+        local eth_device = nil
+        local wifi_device = nil
+        for k, _ in pairs(net_now.devices) do
+            local first_char = string.sub(k, 1, 1)
+            if first_char == 'e' then
+                eth_device = k
+            elseif first_char == 'w' then
+                wifi_device = k
+            end
         end
+        local net_status = ''
+        local eth = net_now.devices[eth_device]
+        if eth and eth.ethernet and eth.carrier == '1' then
+            local eth_label = markup.fg.color(label_color, eth_device)
+            net_status = net_status .. eth_label .. ' ↓' .. eth.received .. '↑' .. eth.sent
+        end
+        local wlan = net_now.devices[wifi_device]
+        if wlan and wlan.wifi then
+            if wlan.carrier == '1' then
+                local wifi_label = markup.fg.color(label_color, wifi_device)
+                net_status = net_status .. wifi_label .. ' ↓' .. wlan.received .. '↑' .. wlan.sent
+            end
+        end
+        widget:set_markup(net_status)
     end,
 }
+local lain_bat = lain.widget.bat {
+    notify = 'off',
+    battery = 'BAT1',
+    settings = function()
+        local ac = ''
+        if bat_now.status == 'Charging' then
+            ac = '↑'
+        end
+        local bat_label = markup.fg.color(label_color, 'bat')
+        local bat_value = bat_now.perc
+        if bat_value < 30 then
+            bat_value = markup.fg.color(alert_color, bat_value)
+        end
+        widget:set_markup(bat_label .. ' ' .. bat_value .. '%' .. ac)
+    end,
+}
+local updates_widget = awful.widget.watch('showupdates', 600)
+local wifi_ssid_widget = awful.widget.watch('iwgetid -r', 5)
 
 -- Create a wibox for each screen and add it
 local function set_wallpaper(s)
@@ -176,17 +238,22 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            wibox.widget.textbox ' ',
+            wifi_ssid_widget,
+            wibox.widget.textbox '  ',
             lain_net.widget,
-            wibox.widget.textbox ' |',
+            wibox.widget.textbox '  ',
             lain_cpu.widget,
-            wibox.widget.textbox '|',
+            wibox.widget.textbox '  ',
             lain_mem.widget,
-            wibox.widget.textbox '|',
+            wibox.widget.textbox '  ',
             lain_fs.widget,
-            wibox.widget.textbox '|',
+            wibox.widget.textbox '  ',
+            lain_bat.widget,
+            wibox.widget.textbox(markup.fg.color(sep_color, ' | ')),
             mytextclock,
-            wibox.widget.textbox '| ',
-            awful.widget.watch('showupdates', 600),
+            wibox.widget.textbox(markup.fg.color(sep_color, ' | ')),
+            updates_widget,
             wibox.widget.textbox ' ',
             s.mylayoutbox,
         },
@@ -216,10 +283,14 @@ globalkeys = gears.table.join(
         awful.client.swap.byidx(-1)
     end, { description = 'swap with previous client by index', group = 'client' }),
     awful.key({ modkey }, 'e', function()
-        awful.screen.focus_relative(1)
+        if awful.screen.focused({}).index < screen:count() then
+            awful.screen.focus_relative(1)
+        end
     end, { description = 'focus the next screen', group = 'screen' }),
     awful.key({ modkey }, 'w', function()
-        awful.screen.focus_relative(-1)
+        if awful.screen.focused({}).index > 1 then
+            awful.screen.focus_relative(-1)
+        end
     end, { description = 'focus the previous screen', group = 'screen' }),
     awful.key({ modkey }, 'u', awful.client.urgent.jumpto, { description = 'jump to urgent client', group = 'client' }),
     awful.key({ modkey }, 'Tab', function()
