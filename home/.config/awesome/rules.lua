43a3d383198dce8ad7c62c95a23dda1e231b56fa
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2024-12-05 07:52:57 -0300

    4k and nvidia config, solaar floating

--- home/.config/awesome/rules.lua
+++ home/.config/awesome/rules.lua
@@ -41,6 +41,7 @@ awful.rules.rules = {
                 'Wpa_gui',
                 'veromix',
                 'xtightvncviewer',
+                'Solaar',
                 'steam',
             },
 
