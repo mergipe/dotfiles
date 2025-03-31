c4f6a6f9570c8ddc66176f1c8a88bf1a4d7fba18
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2025-03-31 20:10:09 -0300

    nvim and stylua config

--- home/.config/nvim/lua/custom/plugins/lsp_signature.lua
+++ home/.config/nvim/lua/custom/plugins/lsp_signature.lua
@@ -1,28 +1,29 @@
 return {
   {
-    'ray-x/lsp_signature.nvim',
+    "ray-x/lsp_signature.nvim",
     -- event = 'BufEnter',
     opts = {
       bind = true,
-      doc_lines = 0,
+      doc_lines = 10,
+      max_height = 12,
       max_width = 110,
       floating_window = false,
       hint_enable = false,
       hint_prefix = {
-        above = '↙ ', -- when the hint is on the line above the current line
-        current = '← ', -- when the hint is on the same line
-        below = '↖ ', -- when the hint is on the line below the current line
+        above = "↙ ", -- when the hint is on the line above the current line
+        current = "← ", -- when the hint is on the same line
+        below = "↖ ", -- when the hint is on the line below the current line
       },
       handler_opts = {
-        border = 'none',
+        border = "rounded",
       },
-      padding = '',
-      toggle_key = '<C-p>',
+      padding = "",
+      toggle_key = "<C-p>",
       toggle_key_flip_floatwin_setting = true,
       -- select_signature_key = '<C-n>',
     },
     config = function(_, opts)
-      require('lsp_signature').setup(opts)
+      require("lsp_signature").setup(opts)
     end,
   },
 }
